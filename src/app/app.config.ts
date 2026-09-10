91620842814564989cd3feb92f303e07029e7de0
Brodt,Jason (HR Appr) BIP-DE-B <jason.brodt@boehringer-ingelheim.com>
2026-09-10 11:54:50 +0200

    refactor: remove server-side rendering configuration and related dependencies
    - Deleted app.config.server.ts and app.routes.server.ts files to eliminate server-side rendering setup.
    - Removed @angular/platform-server and @angular/ssr dependencies from package.json.
    - Updated app.config.ts to remove client hydration and changed service worker registration strategy to 'registerImmediately'.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,16 +4,14 @@ import { isDevMode } from '@angular/core';
 import { provideServiceWorker } from '@angular/service-worker';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideClientHydration(),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
-      registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: 'registerImmediately'
     })
   ],
 };
